41074ad12ddd11b29732bbb9e3252e0b6e328ff6
DF1E <dnielfe@gmail.com>
2013-08-03 19:39:02 +0200

    settings update 3

---
+++ src/com/dnielfe/manager/SearchSuggestions.java
@@ -0,0 +1,12 @@
+package com.dnielfe.manager;
+
+import android.content.SearchRecentSuggestionsProvider;
+
+public class SearchSuggestions extends SearchRecentSuggestionsProvider {
+	public final static String AUTHORITY = "com.dnielfe.manager";
+	public final static int MODE = DATABASE_MODE_QUERIES;
+
+	public SearchSuggestions() {
+		setupSuggestions(AUTHORITY, MODE);
+	}
+}
