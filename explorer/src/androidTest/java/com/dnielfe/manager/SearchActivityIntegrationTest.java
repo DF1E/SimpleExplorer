8decbac0bc39d39cbdfa7ec70aa839c334882951
Geonu Kang <blamethegnu@unrulygnu.com>
2015-05-14 10:29:54 +0900

    Integrate Testing Support Library for Android Instrumentation tests
    Updated build.gradle to accommodate dependencies.
    
    Updated SearchActivityIntegrationTest to use the new test runner
    and Espresso.

--- explorer/src/androidTest/java/com/dnielfe/manager/SearchActivityIntegrationTest.java
+++ explorer/src/androidTest/java/com/dnielfe/manager/SearchActivityIntegrationTest.java
@@ -1,10 +1,21 @@
 package com.dnielfe.manager;
 
 import android.app.Instrumentation;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.KeyEvent;
-import android.view.View;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
 public class SearchActivityIntegrationTest
         extends ActivityInstrumentationTestCase2<BrowserActivity> {
     private Instrumentation mInstrumentation;
@@ -15,26 +26,30 @@ public class SearchActivityIntegrationTest
         super(BrowserActivity.class);
     }
 
-    @Override
-    protected void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
         setActivityInitialTouchMode(false);
         mInstrumentation = getInstrumentation();
-        mBrowserActivity = getActivity();
+        mBrowserActivity = this.getActivity();
         mSearchActivity = getSearchActivityByMenuClick();
     }
 
-    @Override
-    protected void tearDown() throws Exception {
+    @After
+    public void tearDown() throws Exception {
         mSearchActivity.finish();
         mBrowserActivity.finish();
+        super.tearDown();
     }
 
+    @Test
     public void testPreconditions() {
         assertTrue(mSearchActivity.hasWindowFocus());
         assertFalse(mBrowserActivity.hasWindowFocus());
     }
 
+    @Test
     public void testNavBack_ByBackButton_DisplaysBrowserActivity() {
         this.sendKeys(KeyEvent.KEYCODE_BACK);
         mInstrumentation.waitForIdleSync();
@@ -45,14 +60,8 @@ public class SearchActivityIntegrationTest
     private SearchActivity getSearchActivityByMenuClick() {
         Instrumentation.ActivityMonitor activityMonitor =
                 mInstrumentation.addMonitor(SearchActivity.class.getName(), null, false);
-        mBrowserActivity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                View searchMenuItem = mBrowserActivity.findViewById(R.id.search);
-                assertNotNull(searchMenuItem);
-                searchMenuItem.performClick();
-            }
-        });
+        onView(withId(R.id.search))
+                .perform(click());
         SearchActivity searchActivity =
                 (SearchActivity) activityMonitor.waitForActivityWithTimeout(2000);
         mInstrumentation.removeMonitor(activityMonitor);
